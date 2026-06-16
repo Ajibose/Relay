15b1571f206b8f0936c77d626225bb6a8c024164
iajibose <ajiboseibrahim12@gmail.com>
2026-06-16 11:03:19 +0100

    Milestone1 - single request tunnel

---
+++ cmd/relayd.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+	cListener, cErr := net.Listen("tcp", ":5000")
+	if cErr != nil {
+		fmt.Println("Failed to create Listener", cErr)
+		return
+	}
+	fmt.Println("Client Server listening on: ", 5000)
+	defer cListener.Close()
+
+	vListener, vErr := net.Listen("tcp", ":5001")
+	if vErr != nil {
+		fmt.Println("Failed to create Listener", vErr)
+		return
+	}
+	fmt.Println("Visitor Server listening on: ", 5001)
+	defer vListener.Close()
+
+	clientConn, clientErr := cListener.Accept()
+	if clientErr != nil {
+		fmt.Println("Connection error", clientErr)
+		return
+	}
+
+	visitorConn, visitorErr := vListener.Accept()
+	if visitorErr != nil {
+		fmt.Println("Connection error", visitorErr)
+		return
+	}
+
+	go Forward(visitorConn, clientConn)
+	Forward(clientConn, visitorConn)
+}
+
+func Forward(conn1 net.Conn, conn2 net.Conn) {
+
+	buffer := make([]byte, 2048)
+	for {
+		n, err := conn1.Read(buffer)
+		if err != nil {
+			fmt.Println("Error Reading From Connection: ", conn1)
+			return
+		}
+
+		_, err = conn2.Write(buffer[:n])
+		if err != nil {
+			fmt.Println("Error Writing to Connection: ", conn2)
+			return
+		}
+	}
+}
